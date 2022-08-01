36e5991345e9724657b65f6629ed56e6f2ddedaa
emredogan <emredogan@outlook.com>
2022-08-02 00:33:40 +0200

    Initial Commit

---
+++ SwiftUI_Native_Interface/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Native_Interface
+//
+//  Created by Emre Dogan on 02/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Native_Interface/SwiftUI_Native_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Native_InterfaceApp.swift
+//  SwiftUI_Native_Interface
+//
+//  Created by Emre Dogan on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Native_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
